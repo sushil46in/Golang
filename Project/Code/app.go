eebb463790edc1f68a2b01738499c5fabdcb965b
sushil46in <58732294+sushil46in@users.noreply.github.com>
2021-02-09 12:06:42 +0100

    sd

--- Project/Code/app.go
+++ Project/Code/app.go
@@ -6,7 +6,7 @@ import (
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello World</h1>")
+	fmt.Fprintf(w, "<h1>Hello to Sushil World</h1>")
 }
 
 func check(w http.ResponseWriter, r *http.Request) {
